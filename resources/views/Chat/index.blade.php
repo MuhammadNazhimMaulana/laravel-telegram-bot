1292051106961defe150c01a18578ac674d56259
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-08-20 08:03:07 +0700

    Finishing View

--- resources/views/Chat/index.blade.php
+++ resources/views/Chat/index.blade.php
@@ -2,8 +2,15 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        Test
+    <div class="card text-center">
+        <div class="card-header text-dark fs-1 fw-bold text-uppercase">
+          {{ __('Welcome') }}
+        </div>
+        <div class="card-body text-dark">
+          <h5 class="card-title text-uppercase fw-bold">{{ __('laraboneapibot') }}</h5>
+          <p class="card-text">{{ __('Aplikasi ini merupakan sebuah aplikasi bot telegram yang dibuat dengan menggunakan Laravel.') }}</p>
+          {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
+        </div>
     </div>
 </div>
 
--- resources/views/layout/main_admin.blade.php
+++ resources/views/layout/main_admin.blade.php
@@ -9,21 +9,21 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
 
     <!-- Link menuju CSS -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
 
     <!-- Bootstrap CSS -->
     <link href="{{ asset('bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <title>App</title>
   </head>
-  <body>
+  <body class="bg-secondary text-white">
 
     <div id="app">
       {{-- Navbar --}}
       {{-- @include('layout.partials.navbar') --}}
 
       {{-- Content --}}
-      <main class="py-4">
+      <main class="py-4 d-flex align-items-center vh-100">
           @yield('content')
       </main>
     </div>
