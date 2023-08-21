d45cc094d9cacd70c5c450ce8ab17c4c2472b179
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-08-21 07:38:16 +0700

    Adding Button

--- resources/views/Chat/index.blade.php
+++ resources/views/Chat/index.blade.php
@@ -9,7 +9,7 @@
         <div class="card-body text-dark">
           <h5 class="card-title text-uppercase fw-bold">{{ __('laraboneapibot') }}</h5>
           <p class="card-text">{{ __('Aplikasi ini merupakan sebuah aplikasi bot telegram yang dibuat dengan menggunakan Laravel.') }}</p>
-          {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
+          <a target="_blank" href="https://t.me/laratele_bone_bot" class="btn btn-primary">{{ __('Mulai Chat') }}</a>
         </div>
     </div>
 </div>
